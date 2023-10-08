a69e7709870658b9dd88d7db081de8fa3c1ea81e
Safwan Elmadani <safwanelmadani@arizona.edu>
2023-10-07 23:54:56 -0700

    luasnip working

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,18 +1,38 @@
 return {
     "L3MON4D3/LuaSnip",
+    dependencies = { 
+        'rafamadriz/friendly-snippets',
+    },
     -- follow latest release.
     version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
     -- install jsregexp (optional!).
     build = "make install_jsregexp",
     config = function()
-        vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
-        vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
+        local ls = require("luasnip")
+        local s = ls.snippet
+        ls.config.set_config({ -- Setting LuaSnip config
+            -- -- Enable autotriggered snippets
+            enable_autosnippets = true,
+            history = true,
+            updateevents = "TextChanged,TextChangedI",
+
+            -- -- Use Tab (or some other key if you prefer) to trigger visual selection
+            store_selection_keys = "<Tab>",
+        })
+        require("luasnip.loaders.from_vscode").lazy_load()
+        require("luasnip.loaders.from_snipmate").lazy_load()
+
+        vim.keymap.set({"i", "s"}, "<C-K>", function() 
+           if ls.expand_or_jumpable() then
+               ls.expand_or_jump()
+           end
+        end, {silent = true})
         vim.keymap.set({"i", "s"}, "<C-J>", function() ls.jump(-1) end, {silent = true})
 
-        vim.keymap.set({"i", "s"}, "<C-E>", function()
+        vim.keymap.set({"i", "s"}, "<C-L>", function()
             if ls.choice_active() then
                 ls.change_choice(1)
             end
         end, {silent = true})
-    end,
+end,
 }
