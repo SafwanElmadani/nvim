7dbf9133f4b89c0a571fbd4d3fb26ccc0a4b0278
Safwan Elmadani <safwanelmadani@arizona.edu>
2024-03-28 12:45:28 -0700

    update

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -6,4 +6,18 @@ return {
       vim.g.mkdp_filetypes = { "markdown" }
     end,
     ft = { "markdown" },
+    config = function()
+        vim.cmd[[
+            " set to 1, nvim will open the preview window after entering the Markdown buffer
+            " default: 0
+            let g:mkdp_auto_start = 0
+            " set to 1, the nvim will auto close current preview window when changing
+            " from Markdown buffer to another buffer
+            " default: 1
+            let g:mkdp_auto_close = 0
+            " preview page title
+            " ${name} will be replace with the file name
+            let g:mkdp_page_title = '${name}'
+        ]]
+    end,
 }
