fdcf8f30f2634137e2a8b08336f24ce79b783380
Safwan Elmadani <safwanelmadani@arizona.edu>
2024-10-17 21:28:38 -0700

    update arch

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -6,7 +6,11 @@ return {
         "nvim-tree/nvim-web-devicons",
     },
     config = function()
-        require("nvim-tree").setup {}
+        require("nvim-tree").setup {
+            git = {
+                ignore = false,
+              },
+        }
         vim.keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>") -- toggle file explorer
         vim.keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>") -- toggle file explorer
         vim.keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>") -- toggle file explorer
