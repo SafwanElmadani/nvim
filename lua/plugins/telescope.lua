3cf8fb437259d99ab69cc237d25dffcc9eb2749a
Safwan Elmadani <safwanelmadani@arizona.edu>
2024-10-19 13:00:27 -0700

    update arch

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,7 +8,14 @@ return {
     },
     config = function()
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+        -- vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>ff', function()
+            builtin.find_files({
+                hidden = true,
+                no_ignore = true,
+                layout_config = { prompt_position = "top" }
+            })
+        end, {})
         -- vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
         vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
