bd8061a205617350227908ef0fe6e9eb4bacaeaf
Safwan Elmadani <safwanelmadani@ibm.com>
2025-09-07 11:41:43 -0700

    update

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,7 +3,11 @@ return {
     name = "catppuccin",
     priority = 1000,
     config = function()
-        vim.keymap.set("n", "<leader>lt", function() vim.cmd("colorscheme catppuccin-latte") end)
+        -- Keymaps for quick switching
+        vim.keymap.set("n", "<leader>ll", function() vim.cmd.colorscheme("catppuccin-latte") end, { desc = "Latte" })
+        vim.keymap.set("n", "<leader>lf", function() vim.cmd.colorscheme("catppuccin-frappe") end, { desc = "Frappe" })
+        vim.keymap.set("n", "<leader>lm", function() vim.cmd.colorscheme("catppuccin-macchiato") end, { desc = "Macchiato" })
+        vim.keymap.set("n", "<leader>lo", function() vim.cmd.colorscheme("catppuccin-mocha") end, { desc = "Mocha" })
     end
 }
 
