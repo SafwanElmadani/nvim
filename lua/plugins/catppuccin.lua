8a32595898e05a45d1d7af5e760be52446b1885c
Safwan Elmadani <safwanelmadani@ibm.com>
2025-09-11 09:20:01 -0700

    update

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -8,6 +8,25 @@ return {
         vim.keymap.set("n", "<leader>lf", function() vim.cmd.colorscheme("catppuccin-frappe") end, { desc = "Frappe" })
         vim.keymap.set("n", "<leader>lm", function() vim.cmd.colorscheme("catppuccin-macchiato") end, { desc = "Macchiato" })
         vim.keymap.set("n", "<leader>lo", function() vim.cmd.colorscheme("catppuccin-mocha") end, { desc = "Mocha" })
+
+        -- Highlight overrides
+        local function set_highlights()
+            vim.api.nvim_set_hl(0, "CursorLine",   { bg = "#45475a" })
+            vim.api.nvim_set_hl(0, "CursorColumn", { bg = "#45475a" })
+        end
+
+        -- Apply immediately if a catppuccin-* colorscheme is already loaded
+        if vim.g.colors_name and vim.g.colors_name:match("^catppuccin%-") then
+            set_highlights()
+        end
+
+        -- Re-apply whenever colorscheme changes
+        vim.api.nvim_create_autocmd("ColorScheme", {
+            callback = function(args)
+                if args.match:match("^catppuccin%-") then
+                    set_highlights()
+                end
+            end,
+        })
     end
 }
-
