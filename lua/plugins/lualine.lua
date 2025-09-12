aaf41af42b3ed65bae1956b6f840857d733b78af
Safwan Elmadani <safwanelmadani@ibm.com>
2025-09-11 17:29:47 -0700

    update: changed theme for lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -8,7 +8,7 @@ return {
         require('lualine').setup {
             options = {
                 icons_enabled = true,
-                theme = 'powerline',
+                -- theme = 'powerline',
             }
         }
     end,
