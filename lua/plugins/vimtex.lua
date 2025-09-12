4c98c3316feedc3096a74f7512db827af529c7ab
Safwan Elmadani <safwanelmadani@ibm.com>
2025-09-11 22:21:44 -0700

    update: vimtex

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -3,10 +3,13 @@ return{
     lazy = false,
     init = function()
         vim.g.vimtex_syntax_enabled = 1
-        vim.g.vimtex_view_method = 'zathura'
+        -- vim.g.vimtex_view_method = 'zathura'
+        vim.g.vimtex_view_method = 'sioyek'
         vim.g.vimtex_view_general_viewer = 'okular'
         vim.g.vimtex_view_general_options = [[--unique file:@pdf\#src:@line@tex]]
         vim.g.vimtex_quickfix_enabled = 1
         vim.g.vimtex_quickfix_mode = 0
+
+        vim.g.maplocalleader = " "
     end,
 }
