43cd7004eb7aadd8051fd0521f3f15929a669c50
Safwan Elmadani <safwanelmadani@ibm.com>
2025-09-14 15:52:50 -0700

    update: vimtex

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -9,7 +9,15 @@ return{
         vim.g.vimtex_view_general_options = [[--unique file:@pdf\#src:@line@tex]]
         vim.g.vimtex_quickfix_enabled = 1
         vim.g.vimtex_quickfix_mode = 0
-
-        vim.g.maplocalleader = " "
+    
+        -- default leader is '\'
+        -- vim.g.maplocalleader = " "
+        -- to start or stop compiling: \ll'
+        -- jump to the line in the pdf: \lv
+        -- to clean: \lc
+        -- show errors/warnings in quickfix list: \le
+        -- To compile manually:
+            -- latexmk -pdf myfile.tex
+            -- to clean: latexmk -c
     end,
 }
